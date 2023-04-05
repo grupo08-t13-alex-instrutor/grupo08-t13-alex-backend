bdf8cba27aa47a5805dd1f8c3eae8353de4dafea
davi894 <moisesdavi005@gmail.com>
2023-04-05 15:49:43 -0300

    feat: Adiciona estrutura inicial do projeto

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import "express-async-errors";
+import express from "express";
+import "reflect-metadata";
+import cors from "cors"
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+export default app;
---
+++ src/data-source.ts
@@ -0,0 +1,26 @@
+import "dotenv/config";
+import path from "path";
+import "reflect-metadata";
+import { DataSource, DataSourceOptions } from "typeorm";
+
+const dataSourceConfig = (): DataSourceOptions => {
+  const entitiesPath: string = path.join(__dirname, "./entities/*.{js,ts}");
+  const migrationsPath: string = path.join(__dirname, "./migrations/*.{js,ts}");
+
+  return {
+    type: "postgres",
+    host: process.env.PGHOST,
+    port: parseInt(process.env.PGPORT!),
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    logging: false,
+    entities: [entitiesPath],
+    migrations: [migrationsPath],
+    synchronize: false,
+  };
+};
+
+const AppDataSource = new DataSource(dataSourceConfig());
+
+export { AppDataSource };
