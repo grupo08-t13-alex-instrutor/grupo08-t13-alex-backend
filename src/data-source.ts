b26507ee21042863f6cbe8bfa8068fe07482ecd9
davi894 <moisesdavi005@gmail.com>
2023-04-06 09:36:16 -0300

    chore: restaurando estrutura do projeto

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,11 +1,10 @@
 import "dotenv/config";
-import path from "path";
 import "reflect-metadata";
 import { DataSource, DataSourceOptions } from "typeorm";
 
 const dataSourceConfig = (): DataSourceOptions => {
-  const entitiesPath: string = path.join(__dirname, "./entities/*.{js,ts}");
-  const migrationsPath: string = path.join(__dirname, "./migrations/*.{js,ts}");
+  const entities = []
+  const migrations = []
 
   return {
     type: "postgres",
@@ -15,8 +14,8 @@ const dataSourceConfig = (): DataSourceOptions => {
     password: process.env.PGPASSWORD,
     database: process.env.PGDATABASE,
     logging: false,
-    entities: [entitiesPath],
-    migrations: [migrationsPath],
+    entities: entities,
+    migrations: migrations,
     synchronize: false,
   };
 };
