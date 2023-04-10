d308237c6c320600da00e409fcffb8262b745644
Leonardo Miranda do Nascimento <leonardo.miranda.3@hotmail.com>
2023-04-10 15:19:22 -0400

    Fix: rename files

--- src/routes/announcement.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { deleteAnnouncementController } from "../controllers/announcement.controllers";
-
-const announcementRoutes = Router();
-
-announcementRoutes.delete('/:announcementId', deleteAnnouncementController);
-
-export { announcementRoutes };
