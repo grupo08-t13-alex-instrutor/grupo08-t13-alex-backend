da392d4113a242eb217b88912531a554ac2bb636
davi894 <moisesdavi005@gmail.com>
2023-04-18 02:06:18 -0300

    fix: corrigindo importacoes de nomes de variaveis

--- src/services/user/userDelet.service.ts
+++ src/services/user/userDelet.service.ts
@@ -1,12 +1,12 @@
 import { AppDataSource } from "../../data-source";
 import { User } from "../../entities/users.entity";
-import AppError from "../../erros/AppError"
+import AppError from "../../errors/AppError"
 
-const userDeletService = async (idUser) => {
+const userDeletService = async (idUser: string) => {
 
     const userRespository = AppDataSource.getRepository(User);
     const findUser = await userRespository.findOneBy({ id: idUser });
-    
+
     if (!findUser) {
         throw new AppError(404, "User not exists!");
     };
--- src/services/user/userPatch.service.ts
+++ src/services/user/userPatch.service.ts
@@ -1,35 +1,36 @@
+import { hashSync } from "bcryptjs";
 import { AppDataSource } from "../../data-source";
 import { User } from "../../entities/users.entity";
-import AppError from "../../erros/AppError"
+import AppError from "../../errors/AppError"
 
 
-const userPatchService = async ( dataUpdate, idUser ) => {
+const userPatchService = async (dataUpdate, idUser: string) => {
 
     const userRespository = AppDataSource.getRepository(User);
     const findUser = await userRespository.findOneBy({ id: idUser });
-    
-    if ( !findUser ) {
+
+    if (!findUser) {
         throw new AppError(404, "User not exists!");
     };
 
-    if( dataUpdate.password ){
+    if (dataUpdate.password) {
 
         const updateUser = userRespository.create({
-            ...foundUser,
+            ...findUser,
             ...dataUpdate,
             password: hashSync(`${dataUpdate.password}`, 10),
         });
-        
+
         return await userRespository.save(updateUser);
     }
 
     const updateUser = userRespository.create({
-        ...foundUser,
+        ...findUser,
         ...dataUpdate,
     });
-    
+
     const saveDataUpdateUser = await userRespository.save(updateUser);
-    
+
     return saveDataUpdateUser;
 }
 
