bda5b401a9f5e298cfef79b399e0372c2e7d9996
Anibal <anibal4g@gmail.com>
2023-04-18 01:59:42 -0300

    Feat: listing an user

---
+++ src/controllers/userControllers/listAnUser.controller.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express';
+import { listAnUserService } from '../../services/userServices/listAnUser.service';
+
+export const listAnUserController = async (req: Request, res: Response) => {
+    const users = await listAnUserService(req.params.userId)
+    return res.status(200).json(users)
+}
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import User from "../entities/users.entity";
 import { createUserController } from "../controllers/userControllers/createUser.controller";
+import { listAnUserController } from "../controllers/userControllers/listAnUser.controller";
 
 const userRouter = Router();
 
 userRouter.post('', createUserController)
+userRouter.get('/:userId', listAnUserController)
 
 export default userRouter
