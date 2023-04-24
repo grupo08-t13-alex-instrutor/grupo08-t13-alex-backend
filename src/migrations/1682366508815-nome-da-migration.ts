0c74485a6618c5ada8045feeb0b011a2686bb967
Lucas Medina <81653299+lucaspmedina@users.noreply.github.com>
2023-04-24 17:03:58 -0300

    fix: config para deploy no render

---
+++ src/migrations/1682366508815-nome-da-migration.ts
@@ -0,0 +1,72 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class NomeDaMigration1682366508815 implements MigrationInterface {
+    name = 'NomeDaMigration1682366508815'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "adresses" DROP CONSTRAINT "FK_b4f5c94493f23641866f161e212"`);
+        await queryRunner.query(`ALTER TABLE "comments" DROP CONSTRAINT "FK_7e8d7c49f218ebb14314fdb3749"`);
+        await queryRunner.query(`ALTER TABLE "comments" DROP CONSTRAINT "FK_889b53a3793fc35778644069dc7"`);
+        await queryRunner.query(`ALTER TABLE "images" DROP CONSTRAINT "FK_ae189a61e74b46c52bbc144affa"`);
+        await queryRunner.query(`ALTER TABLE "adverts" DROP CONSTRAINT "FK_80a023b502ad7957fb8d470e4e2"`);
+        await queryRunner.query(`ALTER TABLE "images" RENAME COLUMN "advertisemmentId" TO "advertisementId"`);
+        await queryRunner.query(`ALTER TABLE "adresses" DROP COLUMN "road"`);
+        await queryRunner.query(`ALTER TABLE "adresses" DROP COLUMN "userId"`);
+        await queryRunner.query(`ALTER TABLE "adresses" ADD "street" character varying(150) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "buyer" boolean NOT NULL DEFAULT true`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "addressId" uuid`);
+        await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "UQ_bafb08f60d7857f4670c172a6ea" UNIQUE ("addressId")`);
+        await queryRunner.query(`ALTER TABLE "adresses" DROP COLUMN "cep"`);
+        await queryRunner.query(`ALTER TABLE "adresses" ADD "cep" character varying(9) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "UQ_230b925048540454c8b4c481e1c"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "cpf"`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "cpf" character varying(14) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "UQ_230b925048540454c8b4c481e1c" UNIQUE ("cpf")`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "telephone"`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "telephone" character varying(14) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "date_of_birth"`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "date_of_birth" character varying(10) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "adverts" DROP COLUMN "fuel"`);
+        await queryRunner.query(`ALTER TABLE "adverts" ADD "fuel" integer NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "adverts" ALTER COLUMN "published" SET DEFAULT true`);
+        await queryRunner.query(`ALTER TABLE "comments" ADD CONSTRAINT "FK_7e8d7c49f218ebb14314fdb3749" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "comments" ADD CONSTRAINT "FK_889b53a3793fc35778644069dc7" FOREIGN KEY ("advertisementId") REFERENCES "adverts"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "FK_bafb08f60d7857f4670c172a6ea" FOREIGN KEY ("addressId") REFERENCES "adresses"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "images" ADD CONSTRAINT "FK_b70fa3486870bb59477e9505ca0" FOREIGN KEY ("advertisementId") REFERENCES "adverts"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "adverts" ADD CONSTRAINT "FK_80a023b502ad7957fb8d470e4e2" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "adverts" DROP CONSTRAINT "FK_80a023b502ad7957fb8d470e4e2"`);
+        await queryRunner.query(`ALTER TABLE "images" DROP CONSTRAINT "FK_b70fa3486870bb59477e9505ca0"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "FK_bafb08f60d7857f4670c172a6ea"`);
+        await queryRunner.query(`ALTER TABLE "comments" DROP CONSTRAINT "FK_889b53a3793fc35778644069dc7"`);
+        await queryRunner.query(`ALTER TABLE "comments" DROP CONSTRAINT "FK_7e8d7c49f218ebb14314fdb3749"`);
+        await queryRunner.query(`ALTER TABLE "adverts" ALTER COLUMN "published" DROP DEFAULT`);
+        await queryRunner.query(`ALTER TABLE "adverts" DROP COLUMN "fuel"`);
+        await queryRunner.query(`ALTER TABLE "adverts" ADD "fuel" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "date_of_birth"`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "date_of_birth" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "telephone"`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "telephone" integer NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "UQ_230b925048540454c8b4c481e1c"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "cpf"`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "cpf" character varying(11) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "UQ_230b925048540454c8b4c481e1c" UNIQUE ("cpf")`);
+        await queryRunner.query(`ALTER TABLE "adresses" DROP COLUMN "cep"`);
+        await queryRunner.query(`ALTER TABLE "adresses" ADD "cep" character varying(8) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "UQ_bafb08f60d7857f4670c172a6ea"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "addressId"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "buyer"`);
+        await queryRunner.query(`ALTER TABLE "adresses" DROP COLUMN "street"`);
+        await queryRunner.query(`ALTER TABLE "adresses" ADD "userId" uuid`);
+        await queryRunner.query(`ALTER TABLE "adresses" ADD "road" character varying(150) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "images" RENAME COLUMN "advertisementId" TO "advertisemmentId"`);
+        await queryRunner.query(`ALTER TABLE "adverts" ADD CONSTRAINT "FK_80a023b502ad7957fb8d470e4e2" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "images" ADD CONSTRAINT "FK_ae189a61e74b46c52bbc144affa" FOREIGN KEY ("advertisemmentId") REFERENCES "adverts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "comments" ADD CONSTRAINT "FK_889b53a3793fc35778644069dc7" FOREIGN KEY ("advertisementId") REFERENCES "adverts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "comments" ADD CONSTRAINT "FK_7e8d7c49f218ebb14314fdb3749" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "adresses" ADD CONSTRAINT "FK_b4f5c94493f23641866f161e212" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+}
