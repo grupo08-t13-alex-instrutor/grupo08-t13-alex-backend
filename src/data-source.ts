520aa1b48b87456689213dbaca48768f3b2b49f6
Lucas Medina <81653299+lucaspmedina@users.noreply.github.com>
2023-04-24 16:46:59 -0300

    fix: config para deploy no render

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,47 +1,46 @@
 import "dotenv/config";
+import path from "path";
 import "reflect-metadata";
-import path from 'path';
 import { DataSource, DataSourceOptions } from "typeorm";
 import { Address } from "./entities/adresses.entity";
-import { User } from "./entities/users.entity";
 import { Advertisement } from "./entities/adverts.entity";
 import { Comment } from "./entities/comments.entity";
 import { Image } from "./entities/images.entity";
+import { User } from "./entities/users.entity";
+import { NomeDaMigration1682365440584 } from "./migrations/1682365440584-nome-da-migration";
 
 const dataSourceConfig = (): DataSourceOptions => {
-  const entities = [Address, Advertisement, Comment, Image, User];
-  const migrations = [
-  ];
+    const entities = [Address, Advertisement, Comment, Image, User];
+    const migrations = [NomeDaMigration1682365440584];
 
-  const entitiesPath: string = path.join(__dirname, './entities/*.{js,ts}');
-  const migrationsPath: string = path.join(
-      __dirname,
-      './migrations/*.{js,ts}'
-  );
+    const entitiesPath: string = path.join(__dirname, "./entities/*.{js,ts}");
+    const migrationsPath: string = path.join(
+        __dirname,
+        "./migrations/*.{js,ts}"
+    );
 
-  const nodeEnv = process.env.NODE_ENV;
+    const nodeEnv = process.env.NODE_ENV;
 
-
-  if (nodeEnv === 'production') {
+    if (nodeEnv === "production") {
+        return {
+            type: "postgres",
+            url: process.env.DATABASE_URL,
+            entities: [entitiesPath],
+            migrations: [migrationsPath],
+        };
+    }
     return {
-        type: 'postgres',
-        url: process.env.DATABASE_URL,
-        entities: [entitiesPath],
-        migrations: [migrationsPath],
+        type: "postgres",
+        host: process.env.PGHOST,
+        port: parseInt(process.env.PGPORT!),
+        username: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        database: process.env.PGDATABASE,
+        logging: false,
+        entities: entities,
+        migrations: migrations,
+        synchronize: false,
     };
-}
-  return {
-    type: "postgres",
-    host: process.env.PGHOST,
-    port: parseInt(process.env.PGPORT!),
-    username: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
-    logging: false,
-    entities: entities,
-    migrations: migrations,
-    synchronize: false,
-  };
 };
 
 const AppDataSource = new DataSource(dataSourceConfig());
