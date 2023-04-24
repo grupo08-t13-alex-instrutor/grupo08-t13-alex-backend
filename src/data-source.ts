78714257bd72636744459d607fc932a3ae1cba4e
Lucas Medina <81653299+lucaspmedina@users.noreply.github.com>
2023-04-24 16:28:05 -0300

    fix: config para deploy no render

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import "reflect-metadata";
+import path from 'path';
 import { DataSource, DataSourceOptions } from "typeorm";
 import { Address } from "./entities/adresses.entity";
 import { User } from "./entities/users.entity";
@@ -12,6 +13,23 @@ const dataSourceConfig = (): DataSourceOptions => {
   const migrations = [
   ];
 
+  const entitiesPath: string = path.join(__dirname, './entities/*.{js,ts}');
+  const migrationsPath: string = path.join(
+      __dirname,
+      './migrations/*.{js,ts}'
+  );
+
+  const nodeEnv = process.env.NODE_ENV;
+
+
+  if (nodeEnv === 'production') {
+    return {
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        entities: [entitiesPath],
+        migrations: [migrationsPath],
+    };
+}
   return {
     type: "postgres",
     host: process.env.PGHOST,
