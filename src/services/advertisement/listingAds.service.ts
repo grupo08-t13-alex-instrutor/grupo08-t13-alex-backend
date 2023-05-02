4b2ce6c41527f54c256e43cc7625dcfe20f4ced3
davi894 <moisesdavi005@gmail.com>
2023-05-02 10:19:49 -0300

    feat: adicioando paginacao na listagem de anuncios

--- src/services/advertisement/listingAds.service.ts
+++ src/services/advertisement/listingAds.service.ts
@@ -1,20 +1,51 @@
 import { AppDataSource } from '../../data-source';
 import { Advertisement } from '../../database/entities/adverts.entity';
-import { IAdResponse } from '../../interfaces/Ads/response';
 import { listingAdsSerializer } from '../../serializers/Ads/ads.serializers';
 
-const listingAdsService = async (): Promise<IAdResponse[]> => {
+const PAGE_SIZE = 9
+
+const listingAdsService = async (page: any) => {
     const listingAdsRepository = AppDataSource.getRepository(Advertisement);
+
+    if (page) {
+        const pageTransformNumber = parseInt(page)
+
+        const offset = (pageTransformNumber) * PAGE_SIZE;
+
+        const advertisements = await listingAdsRepository
+            .createQueryBuilder('advertisement')
+            .leftJoinAndSelect('advertisement.images', 'image')
+            .leftJoinAndSelect('advertisement.user', 'user')
+            .offset(offset)
+            .limit(PAGE_SIZE)
+            .orderBy("advertisement.price", "ASC")
+            .getMany()
+
+
+        const validatedDataResponse = await listingAdsSerializer.validate(advertisements, { stripUnknown: true });
+
+
+        const totalPage = await listingAdsRepository.count()
+
+        const response = {
+            ads: validatedDataResponse,
+            page: pageTransformNumber,
+            totalPage: (Math.ceil(totalPage / PAGE_SIZE))
+        };
+
+        return response;
+    }
 
     const advertisements = await listingAdsRepository
         .createQueryBuilder('advertisement')
         .leftJoinAndSelect('advertisement.images', 'image')
         .leftJoinAndSelect('advertisement.user', 'user')
-        .getMany();
+        .orderBy("advertisement.price", "ASC")
+        .getMany()
 
     const validatedDataResponse = await listingAdsSerializer.validate(advertisements, { stripUnknown: true });
 
-    return validatedDataResponse;
+    return validatedDataResponse
 };
 
 export default listingAdsService;
